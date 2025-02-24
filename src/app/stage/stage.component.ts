1c1d693c498d49601b1544fdda6c46e7a10684d6
jackpunt <jpeckj@gmail.com>
2025-02-23 21:55:32 -0800

    qParams include 'title'

--- src/app/stage/stage.component.ts
+++ src/app/stage/stage.component.ts
@@ -56,10 +56,11 @@ export class StageComponent implements OnInit {
   }
 
   ngAfterViewInit2() {
-    let href: string = document.location.href;
-    console.log(stime(this, ".ngAfterViewInit---"), href, "qParams=", this.qParams)
-    const gs = new GameSetup(this.mapCanvasId, this.qParams);    // load images; new GamePlay(qParams);
-    this.titleService.setTitle(`${this.titleService.getTitle()} ${gs.pageLabel}`)
+    const href: string = document.location.href, title = this.titleService.getTitle();
+    const qParams = { ...this.qParams, title };
+    console.log(stime(this, ".ngAfterViewInit---"), href, "qParams=", qParams)
+    const gs = new GameSetup(this.mapCanvasId, qParams);    // load images; new GamePlay(qParams);
+    this.titleService.setTitle(`${title} ${gs.pageLabel}`)
   }
 
   // see: stream-writer.setButton
