b41eb0716e4f0b00162fe7723f2303cbcab4be77
jackpunt <jpeckj@gmail.com>
2026-06-30 14:05:10 -0700

    gulp to inject  asset/image references in app.component.html

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,39 @@
 // coerce /dist files into a clickable app:
 //
-// npm install --save-dev gulp gulp-inline-source gulp-replace
+// npm install --save-dev gulp gulp-replace inline-source-cli
+//
+// inline the asset files, by injecting into HTMLDivElement: canvas-asset-registry
+// scan the asset/images directory and slurp in each .png file:
 //
 // ng build --configuration production --base-href ./ && gulp bundle-html
 
+// npx gulp generate-asset-tags && npx ng build --configuration production --base-href ./ && npx inline-source --root ./dist/columns/ ./dist/columns/index.html ./dist-standalone/columns.html
+
 const gulp = require('gulp');
-const inlinesource = require('gulp-inline-source');
+const fs = require('fs');
 const replace = require('gulp-replace');
 
-gulp.task('bundle-html', () => {
-    // Target the index.html directly inside your flat project build folder
-    return gulp.src('./dist/columns/index.html')
-        // Force gulp-inline-source to recognize your specific hashed scripts by adding the inline attribute
-        .pipe(replace('type="module"', 'type="module" inline'))
-        .pipe(replace('rel="stylesheet"', 'rel="stylesheet" inline'))
-        // Execute the script and style asset inlining step
-        .pipe(inlinesource({
-            compress: false,
-            rootpath: './dist/columns/' // Points directly to your root build assets
-        }))
-        // Output the single self-contained index file into your standalone folder
-        .pipe(gulp.dest('./dist-standalone'));
+gulp.task('generate-asset-tags', (done) => {
+    const assetsDir = './src/assets/';
+
+    fs.readdir(assetsDir, (err, files) => {
+        if (err) return done(err);
+
+        // Map every asset file into an image tag marked with the inline attribute
+        const imgTags = files
+            .filter(file => /\.(png|jpg|jpeg|ico)$/i.test(file))
+            .map(file => `<img id="asset-${file.split('.')[0]}" src="assets/${file}" inline style="display:none;" />`)
+            .join('\n');
+
+        // Reconstruct the full element housing your generated collection
+        const updatedRegistryBlock = `<div id="assets-image-registry" style="display:none;">\n${imgTags}\n</div>`;
+
+        // Match the element container and anything inside it (including newlines)
+        const searchRegex = /<div id="assets-image-registry"[\s\S]*?<\/div>/;
+
+        gulp.src('./src/app/app.component.html')
+            .pipe(replace(searchRegex, updatedRegistryBlock))
+            .pipe(gulp.dest('./src/app/'))
+            .on('end', done);
+    });
 });
